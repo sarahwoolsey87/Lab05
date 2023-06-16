4fbae2a66a21ee72c752e693a66c6b3c0110f870
sarahwoolsey87 <shedontusejelly0@gmail.com>
2023-06-16 10:48:08 +0100

    1 and 2

--- js/app.js
+++ js/app.js
@@ -1,4 +1,7 @@
 'use strict';
+
+console.log ("Testing")
+
 /////////////////////////////////////
 /* Problem 1 (this is your demo that we'll solve in class)
 Write a function called sum() that takes in two numbers as arguments and then returns an array where the first element is the sum of those numbers, and the second element is a concatenated string that EXACTLY follows this example and uses the values that were input into the function:
@@ -9,11 +12,16 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sum(a, b) { //eslint-disable-line
-
+    let sumResult = a + b;
+    let sumString = "the sum of" + a + "and" + b + sumResult + "";
+    return [sumResult, sumString];
 }
+console.log(sum(4,7));
 
-// Here is the test for sum(); uncomment it to run it
-// testSum(4, 7);
+
+//I don't understand how the testSum command works - sorry. Does console.log not do the same thing?
+
+//testSum(4, 7);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -27,8 +35,10 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiply(a, b) { //eslint-disable-line
-
+let Result= a * b;
+return Result;
 }
+console.log(multiply(5,9))
 
 // Here is the test for multiply(); uncomment it to run it
 // testMultiply(5,9);
